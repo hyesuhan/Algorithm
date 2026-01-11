196d51e3c41ab994e9a349cbc99dd17830f265c4
혜수 <121243582+hyesuhan@users.noreply.github.com>
2026-01-11 12:49:32 +0900

    [Silver III] Title: N과 M (4), Time: 200 ms, Memory: 21248 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main{
+    static int N, M;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        N = sc.nextInt();
+        M = sc.nextInt();
+        
+        selected = new int[M + 1];
+        
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+    
+    static void rec_func(int k) {
+        if (k == M+1) {
+            // 다 고른 상황
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+        } else {
+	        int start = selected[k-1];
+	        if(start == 0) start = 1; // if the start is 0, then set start to 1
+            for (int cand = start; cand <= N; cand++) {
+		            // if (used[cand] == 1) continue;
+		            // Because Multiset is available
+                selected[k] = cand;
+                
+                rec_func(k+1);
+                
+                selected[k] = 0; // Back-Tracking
+            }
+        }
+    }
+}
