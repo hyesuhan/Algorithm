2ed263ff973ac86f64490c822eeb51ebbf711e98
혜수 <121243582+hyesuhan@users.noreply.github.com>
2026-01-11 12:58:30 +0900

    [Silver III] Title: N과 M (2), Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main{
+    static int N, M;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        N = sc.nextInt();
+        M = sc.nextInt();
+        
+        selected = new int[M + 1];
+        selected[0] = 0;
+        
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+    
+    static void rec_func(int k) {
+        if (k == M+1) {
+            // 다 고른 상황
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+        } else {
+            for (int cand = selected[k-1] + 1; cand <= N; cand++) {
+                selected[k] = cand;
+                
+                rec_func(k+1);
+                selected[k] = 0; // Back-Tracking
+            }
+        }
+    }
+}
